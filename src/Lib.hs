7e342d8d9321fed0d097e2437d132dd5152c44a1
Nick Griffiths <ngriffiths21@gmail.com>
2020-05-01 00:15:12 -0400

    implementation for logistic regression

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,33 @@
 module Lib
-    ( someFunc
+    ( logReg
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Numeric.LinearAlgebra hiding ((<>))
+
+eta :: Matrix Double -> Vector Double -> Vector Double
+eta a x = a #> x
+
+g :: Floating a => a -> a
+g e = 1 / (exp(-e) + 1)
+
+gprime :: Floating a => a -> a
+gprime e = exp(-e) / (exp(-e) + 1)**2
+
+z :: Floating a => a -> a -> a
+z e b = e + (b - g e) / gprime e
+
+w :: Floating a => a -> a
+w e = (gprime e)**2 / ((g e) * (1 - g e))
+
+eqleft :: Matrix Double -> Vector Double -> Matrix Double
+eqleft a x = tr a <> (matrix 1 (toList (w (eta a x))) * a)
+
+eqright :: Matrix Double -> Vector Double -> Vector Double -> Vector Double
+eqright a b x = tr a #> (w (eta a x) * (z (eta a x) b))
+
+doIteration :: Matrix Double -> Vector Double -> Vector Double -> Vector Double
+doIteration a b x = (eqleft a x) <\> (eqright a b x)
+
+logReg :: Matrix Double -> Vector Double -> Vector Double -> [Vector Double]
+logReg a b x = x : logReg a b (doIteration a b x) 
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import Test.Hspec
+import Numeric.LinearAlgebra
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "logReg" $ do
+    it "can perform logistic regression" $ do
+      pending
+
+testA = (4><2) [1::Double, 3, 1, 5, 1, 1, 1, 4]
+testB = vector [1, 0, 0, 1]
+testX = vector [0, 0]
+
